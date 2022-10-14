78a33506990a0b5213b29c70bebb02d238069160
MohamedOmer <workspace.mega@gmail.com>
2022-10-13 17:18:25 -0700

    that is some bullshit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
  /**
- * main - prints text followed by a new line
- * Return: Always 0 (Success)
- */
+  * main - prints exactly "and that piece of art is useful"
+  * - Dora Korpar, 2015-10-19",
+  * followed by a new line, to the standard error.
+  */
 int main(void)
 {
-write(4, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
