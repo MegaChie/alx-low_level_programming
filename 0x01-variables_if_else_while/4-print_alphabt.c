716b5b5d9a01f94e2eadbaa4609219ef529b2031
MohamedOmer <workspace.mega@gmail.com>
2022-10-14 14:36:12 -0700

    high again

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha != 'e' && alpha != 'q'; alpha++)
+	{
+	putchar(alpha);
+	}
+putchar('\n');
+return (0);
+}
