5150116f72e7029428ad4b8361fbc8b5b1788a83
MohamedOmer <workspace.mega@gmail.com>
2022-10-14 16:46:28 -0700

    f*ck

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio>
+/**
+ * main - entry point
+ * return: always 0 (Success)
+int main(void)
+{
+int a;
+int s;
+
+for (a = 48; a < 56; n++)
+{
+for (s = 49; s < 57; s++)
+{
+if (s > a)
+{
+putchar(a);
+putchar(s);
+if (a != 56 || s != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('/n');
+return (0);
+}
