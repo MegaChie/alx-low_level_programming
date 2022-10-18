2dc637269a7c57d279b0fd94bcbe0126d1f0e3c8
MohamedOmer <workspace.mega@gmail.com>
2022-10-18 11:49:24 -0700

    clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+int a;
+int s;
+
+for (i = 0; i < 24; i++)
+	{
+	for (s = 0; s < 60; s++)
+		{
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		_putchar(':');
+		_putchar((s / 10) + '0');
+		_putchar((s % 10) + '0');
+		}
+	}
+}
