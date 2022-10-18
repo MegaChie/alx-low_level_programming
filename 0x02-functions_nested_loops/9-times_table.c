3c13c0e95c854af2d9c3207372573a017bcbfa0c
MohamedOmer <workspace.mega@gmail.com>
2022-10-18 12:20:15 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+int a, s, d;
+
+for (a = 0; a <= 9; a++)
+	{
+	for (s = 0; s <= 9; s++)
+		{
+		d = a * s;
+		if (s == 0)
+			{
+			_putchar(d + '0');
+			}
+		if (d < 10 && s != 0)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			}
+		else
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((d / 10) + '0');
+			_putchar((d % 10) + '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
