c028649338267bc35174ec35fb841b1a46e53157
MohamedOmer <workspace.mega@gmail.com>
2022-10-20 07:19:59 -0700

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,15 +6,16 @@
 void print_line(int n)
 {
 if (n <= 0)
-	{
-	_putchar('\n');
-	}
-else 
-	{
-	int a;
-	for (a = 0; a <= n; a++)
-	{
-	_putchar('_');
-	}
-	}
+{
+_putchar('\n');
+}
+else
+{
+int a;
+
+for (a = 0; a < n; a++)
+{
+_putchar('_');
+}
+}
 }
