8dca049eb36e1ef65c8c4f5eadcef5438ebe3be4
MohamedOmer <workspace.mega@gmail.com>
2022-10-20 05:20:26 -0700

    more loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ * Return 0
+ * return 1
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+folder for ALX project solution for 0x04. C - More functions, more nested loops
+
+C.
+love, me.
