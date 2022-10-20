5b1cb7be9aefc61473a1acb685ef365711de930f
MohamedOmer <workspace.mega@gmail.com>
2022-10-20 11:35:17 -0700

    Square Enix

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_square(int size)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int s;
+int d;
+
+for (d = 0; d < s; d++)
+{
+for (s = 0; s < n; s++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
