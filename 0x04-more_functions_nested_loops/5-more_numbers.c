26a453a0a36277bd15904003cd490d817a045d43
MohamedOmer <workspace.mega@gmail.com>
2022-10-20 15:04:28 -0700

    nums and nums

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,8 @@ for (a = 0; a < 10; a++)
 {
 for (s = 0; s < 15; s++)
 	{
-	_putchar(j / 10 + '0');
-	_putchar(j % 10 + '0');
+	_putchar(s / 10 + '0');
+	_putchar(s % 10 + '0');
 	}
 _putchar('\n');
 }
