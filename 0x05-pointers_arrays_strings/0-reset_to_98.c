1fa4844986afb83c5b0edd6edccf89ef34f55f64
MegaChie <workspace.mega@gmail.com>
2022-10-24 14:51:45 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * reset_to_98 - updates the value of the variable
  * the pointer points to to 98
@@ -6,5 +6,5 @@
  */
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
