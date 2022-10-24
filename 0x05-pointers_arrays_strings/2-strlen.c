d5ed61a3f586599a105ec81cb24b3a9b999c627e
MegaChie <workspace.mega@gmail.com>
2022-10-24 15:11:21 +0200

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * int _strlen - swaps the values of two integers
+ * @s: pointer to first value
+ */
+int _strlen(char *s)
+{
+int n;
+
+for (n = 1; n != '/0'; n++)
+
+{
+    
+_putchar(n);
+
+}
+
+_putchar(n);
+
+}
