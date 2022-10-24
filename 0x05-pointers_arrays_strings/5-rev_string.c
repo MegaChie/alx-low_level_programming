f72275d55564692b1ff82c9e2bde6f3347538a91
MegaChie <workspace.mega@gmail.com>
2022-10-24 15:59:01 +0200

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - swaps the values of two integers
+ * @s: pointer to first value
+ */
+void rev_string(char *s)
+{
+int n, a;
+for (n = 0; s[n] != '\0'; n++)
+
+{
+
+
+}
+for (a = n - 1; a >= 0; a--)
+{
+_putchar(s[a]);
+}
+}
