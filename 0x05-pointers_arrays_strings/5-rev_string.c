0741a239c191bd772377d7091c4bc671b4860dc5
MegaChie <workspace.mega@gmail.com>
2022-10-24 16:05:23 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,15 +5,23 @@
  */
 void rev_string(char *s)
 {
-int n, a;
-for (n = 0; s[n] != '\0'; n++)
+char b;
+	int a, c, d;
 
-{
+	c = 0;
+	d = 0;
 
+	while (s[c] != '\0')
+	{
+		c++;
+	}
 
-}
-for (a = n - 1; a >= 0; a--)
-{
-_putchar(s[a]);
-}
+	d = c - 1;
+
+	for (a = 0; a < c / 2; a++)
+	{
+		b = s[a];
+		s[a] = s[d];
+		s[d--] = b;
+	}
 }
