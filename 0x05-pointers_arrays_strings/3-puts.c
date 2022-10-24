65fb304f0ece541b0ebd1e0939c25a57e4a3afe5
MegaChie <workspace.mega@gmail.com>
2022-10-24 15:35:07 +0200

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - swaps the values of two integers
+ * @str: pointer to first value
+ */
+void _puts(char *str)
+{
+int n;
+
+for (n = 0; str[n] != '\0'; n++)
+
+{
+
+_putchar(s[n]);
+}
+_putchar('\n');
