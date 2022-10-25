d729b60740d1eb7b77ec4945a577343bf4d60d80
MegaChie <workspace.mega@gmail.com>
2022-10-25 23:30:00 +0200

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _atoi - prints n elements of an array of integers, followed by a new
+ * line
+ * @s: pointer for the sor char
+ */
+int _atoi(char *s)
+{
+int n, s, d = 0, g = 0;
+char f;
+
+for (n = 0; s[n] != '\0'; n++)
+{
+}
+for (s = 0; s < n; s++)
+    if (s[s] == '-' || s[s] == '+')
+    {
+        f[g] = s[s];
+    }
+    else if (s[s] >= 0 && s[s] <= 9)
+    {
+        f[g] = s[s];
+        g++;
+    }
+    else if (s[s] == '\0')
+    {}
+    else ((s[s] >= 'a' && s[s] <= 'z') || s[s] >= 'A' && s[s] <= 'Z')
+    {}
+
+}
