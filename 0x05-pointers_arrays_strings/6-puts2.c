3e4ff8f973281e4d99d0d645b1d53d3661679d64
MegaChie <workspace.mega@gmail.com>
2022-10-25 09:15:30 +0200

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - swaps the values of two integers
+ * @s: pointer to first value
+ */
+void puts2(char *str)
+{
+char b;
+	int a, c, d;
+
+	c = 0;
+	d = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	d = c - 1;
+
+	for (a = 0; a < c / 2; a++)
+	{
+		b = s[a];
+		s[a] = s[d];
+		s[d--] = b;
+	}
+}
