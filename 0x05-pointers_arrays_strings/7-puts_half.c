b2c127d1eb35a2dfcea674723267a928ee5d33c4
MegaChie <workspace.mega@gmail.com>
2022-10-25 18:57:03 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,27 +5,27 @@
  */
 void puts_half(char *str)
 {
-    int a, n, i;
+int a, n, i;
 
-    a = 0;
+a = 0;
 
-    while (str[a] != '\0')
-    {
-        a++;
-    }
+while (str[a] != '\0')
+{
+a++;
+}
 
-    if (a % 2 == 0)
-    {
-        for (i = a / 2; str[i] != '\0'; i++)
-        {
-            _putchar(str[i]);
-        }
-    } else if (a % 2)
-    {
-        for (n = (a - 1) / 2; n < a - 1; n++)
-        {
-            _putchar(str[n + 1]);
-        }
-    }
-    _putchar('\n');
+if (a % 2 == 0)
+{
+for (i = a / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+} else if (a % 2)
+{
+for (n = (a - 1) / 2; n < a - 1; n++)
+{
+_putchar(str[n + 1]);
+}
+}
+_putchar('\n');
 }
