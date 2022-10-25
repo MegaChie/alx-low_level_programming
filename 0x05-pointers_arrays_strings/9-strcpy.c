730fd4d0162bdb45db3cec27e52f6da8988af667
MegaChie <workspace.mega@gmail.com>
2022-10-25 20:44:21 +0200

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcpy - prints n elements of an array of integers, followed by a new
+ * line
+ * @a: pointer to first value
+ * @n: number of variables to print
+ * @dest: pointer for the des char
+ * @src: pointer for the sor char
+ */
+char *_strcpy(char *dest, char *src)
+{
+int n, s;
+
+for (n = 0; s[n] != '\0'; n++)
+{
+}
+for (s = 0; s < n; s++)
+{
+dest[s] = src[s];
+}
+return (dest);
+}
