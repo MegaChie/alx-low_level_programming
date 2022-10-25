aace797422d8e7498d3c293557e184ac865ff162
MegaChie <workspace.mega@gmail.com>
2022-10-25 10:23:36 +0200

    update repo

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - swaps the values of two integers
+ * @str: pointer to first value
+ */
+void _puts(char *str)
+{
+int n;
+
+for (n = 0; str[n] != '\0'; n++)
+
+{
+
+_putchar(str[n]);
+}
+_putchar('\n');
+}
