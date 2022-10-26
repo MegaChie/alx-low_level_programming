ad2590e9fa277434115d693bbe47ce4eefc11b67
MegaChie <workspace.mega@gmail.com>
2022-10-26 02:08:13 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,36 +6,39 @@
  */
 int _atoi(char *s)
 {
-int n, a = 0, x, *v = &x;
+int i, d, n, len, f, digit;
 
-for (n = 0; s[n] != '\0'; n++)
-{}
-while (a < n)
-{
-    if (s[a] == '-')
-    {
-        x[v] = s[a];
-        v++;
-        while (s[a++] <= '0' && s[a++] >= '9')
-        {
-            x[v] = s[a];
-            v++;
-        }
-    }
-    else if (s[a] <= '0' && s[a] >= '9')
-    {
-        x[v] = s[a];
-        v++;
-        while (s[a++] <= '0' && s[a++] >= '9')
-        {
-            x[v] = s[a];
-            v++;
-        }
+    i = 0;
+    d = 0;
+    n = 0;
+    len = 0;
+    f = 0;
+    digit = 0;
 
+    while (s[len] != '\0')
+        len++;
+
+    while (i < len && f == 0)
+    {
+        if (s[i] == '-')
+            ++d;
+
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            digit = s[i] - '0';
+            if (d % 2)
+                digit = -digit;
+            n = n * 10 + digit;
+            f = 1;
+            if (s[i + 1] < '0' || s[i + 1] > '9')
+                break;
+            f = 0;
+        }
+        i++;
     }
-    else
-    {}
-    a++;
-}
-return (x);
+
+    if (f == 0)
+        return (0);
+
+    return (n);
 }
