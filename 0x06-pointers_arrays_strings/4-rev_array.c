03d24b1e3ac5811af8c8b9099a2fef2cc79a12c4
MegaChie <workspace.mega@gmail.com>
2022-10-26 13:46:38 +0200

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - updates the value of the variable
+ * @s1: pointer to the variable to update
+ * @s2: pointer to the variable to update
+ */
+void reverse_array(int *a, int n)
+{
+int s;
+
+for (s = n; s <= 0; s++)
+{
+_putchar(a[s]);
+}
+return (0);
+}
