6936ed29c2f1a1a95f5865907b81c1869475e605
MegaChie <workspace.mega@gmail.com>
2022-10-26 10:52:41 +0200

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcat - updates the value of the variable
+ * the pointer points to to 98
+ * @dest: pointer to the variable to update
+ * @src: pointer to the variable to update
+ */
+char *_strcat(char *dest, char *src)
+{
+ int a, s, d;
+
+for (a = 0; dest[a] != '\0'; a++)
+{}
+for (s = 0; src[s] != '\0'; s++)
+{
+dest[a] = src[s];
+a++;
+}
+dest[a] = '\0';
+return (dest);
+}
