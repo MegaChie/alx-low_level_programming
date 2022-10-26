47e5ea70d54c4209bf002a0a177e8ccfcc408dcd
MegaChie <workspace.mega@gmail.com>
2022-10-26 23:46:17 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,17 +3,19 @@
  * _strcmp - compare two strings
  * @s1: pointer to the variable to update
  * @s2: pointer to the variable to update
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 == *s2)
-    {
-        if (*s1 == '\0')
+    while (*s1 == *s2)
         {
-            return (0);
+            if (*s1 == '\0')
+            {
+                return (0);
+            }
+            s1++;
+            s2++;
         }
-        s1++;
-        s2++;
-    }
-    return (*s1 - *s2);
+        return (*s1 - *s2);
 }
