eace0633919af5cf2831dd2a8f6e7453933e4049
MegaChie <workspace.mega@gmail.com>
2022-10-27 01:01:43 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,29 +3,27 @@
  **cap_string - updates the value of the variable
  * @a: pointer to the variable to update
  */
-char *cap_string(char *a)
+char *cap_string(char *s)
 {
 int i, j;
 
-    char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-        '!', '?', '"', '(', ')', '{', '}'};
+char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+'!', '?', '"', '(', ')', '{', '}'};
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-            s[i] -= 32;
-
-        for (j = 0; j < 13; j++)
-        {
-            if (s[i] == spe[j])
-            {
-                if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-                {
-                    s[i + 1] -= 32;
-                }
-            }
-        }
-    }
-
-    return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+for (j = 0; j < 13; j++)
+{
+if (s[i] == spe[j])
+{
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] -= 32;
+}
+}
+}
+}
+return (s);
 }
