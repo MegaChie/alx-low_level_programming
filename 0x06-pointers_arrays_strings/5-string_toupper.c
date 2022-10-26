35c4d1f63f1d88a8c0c89a1ffdc8298cd9a28805
MegaChie <workspace.mega@gmail.com>
2022-10-26 17:30:42 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,7 @@ for (s = 0; a[s] != '\0'; s++)
 {
 if (a[s] >= 'a' && a[s] >= 'z')
 {
-_putchar(s[a] + '32');
+a[s] = a[s] - 32;
+_putchar(a[s]);
 }
-else
-{
-_putchar(s[a]);
-}
-}
-return (0);
 }
