c8c40decf15f55c572070310d165ada3089698c8
MegaChie <workspace.mega@gmail.com>
2022-10-27 00:13:29 +0200

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * _strcmp - updates the value of the variable
- * @s1: pointer to the variable to update
- * @s2: pointer to the variable to update
+ * reverse_array - updates the value of the variable
+ * @a: pointer to the variable to update
+ * @n: number of variables in array
  */
 void reverse_array(int *a, int n)
 {
 int i, j, tmp;
 
-    j = n - 1;
-
-    for (i = 0; i < n / 2; i++)
-    {
-        tmp = a[i];
-        a[i] = a[j];
-        a[j--] = tmp;
-    }
+j = n - 1;
+for (i = 0; i < n / 2; i++)
+{
+tmp = a[i];
+a[i] = a[j];
+a[j--] = tmp;
+}
 }
