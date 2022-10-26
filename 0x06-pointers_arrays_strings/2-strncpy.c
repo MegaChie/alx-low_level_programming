69ddd5fbd9c3f09ce51fc9d0141fcab0d38408e6
MegaChie <workspace.mega@gmail.com>
2022-10-26 11:14:33 +0200

    REPO update

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcat - updates the value of the variable
+ * the pointer points to to 98
+ * @dest: pointer to the variable to update
+ * @src: pointer to the variable to update
+ * @n: number of charecters to copy
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int a, s, d;
+
+for (a = 0; dest[a] != '\0'; a++)
+{}
+for (s = 0; s < n; s++)
+{
+dest[a] = src[n];
+a++;
+}
+dest[a] = '\0';
+return (dest);
+}
