9e8befb786494ed38efcf808a811aefc72a93def
MegaChie <workspace.mega@gmail.com>
2022-10-27 15:58:40 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,18 +6,19 @@
  */
 void print_number(int n)
 {
-unsigned int r;
-if (n < 0)
-{
-r = -n;
-}
-else
-{
-r = n;
-}
-if (r / 10)
-{
-print_number(r / 10);
-_putchar((r % 10) + '0');
-}
+    unsigned int n1;
+
+    if (n < 0)
+    {
+        n1 = -n;
+        _putchar('-');
+    } else
+    {
+        n1 = n;
+    }
+
+    if (n1 / 10)
+        print_number(n1 / 10);
+
+    _putchar((n1 % 10) + '0');
 }
