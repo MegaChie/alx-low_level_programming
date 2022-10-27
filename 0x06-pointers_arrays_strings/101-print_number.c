ef1eafb9d6f4deebb065e331d355067940a42844
MegaChie <workspace.mega@gmail.com>
2022-10-27 16:00:39 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,22 @@
 #include "main.h"
 /**
- * reverse_array - updates the value of the variable
- * @a: pointer to the variable to update
+ * print_number - updates the value of the variable
  * @n: number of variables in array
  */
 void print_number(int n)
 {
-    unsigned int n1;
+unsigned int n1;
 
-    if (n < 0)
-    {
-        n1 = -n;
-        _putchar('-');
-    } else
-    {
-        n1 = n;
-    }
-
-    if (n1 / 10)
-        print_number(n1 / 10);
-
-    _putchar((n1 % 10) + '0');
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{
+n1 = n;
+}
+if (n1 / 10)
+print_number(n1 / 10);
+_putchar((n1 % 10) + '0');
 }
