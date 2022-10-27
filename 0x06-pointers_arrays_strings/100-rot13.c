c2fb5c123a51809cf2fe0501f22baa0bad3aaaa6
MegaChie <workspace.mega@gmail.com>
2022-10-27 16:44:52 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,7 +6,7 @@
  */
 char *rot13(char *s)
 {
-    int i, j;
+   int i, j;
 
     char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
