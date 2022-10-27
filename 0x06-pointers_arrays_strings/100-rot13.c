335e964a6f82980f457f32a2c07c9a0428084f3a
MegaChie <workspace.mega@gmail.com>
2022-10-27 15:20:44 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,21 +4,24 @@
  * @ch: pointer to the variable to update
  * Return: the resulting string
  */
-char *rot13(char *ch);
+char *rot13(char *s);
 {
-char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 int i, j;
-for (i = 0; s[i] != '\0'; i++)
- {
-for (j = 0; a[j] != '\0'; j++)
-{
-if (s[i] == a[j])
-{
-s[i] = b[j];
-break;
-}
-}
-}
-return (ch);
+
+    char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; a[j] != '\0'; j++)
+        {
+            if (s[i] == a[j])
+            {
+                s[i] = b[j];
+                break;
+            }
+        }
+    }
+
+    return (s);
 }
