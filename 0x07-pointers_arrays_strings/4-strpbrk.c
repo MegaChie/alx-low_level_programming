1a34bba1e86e3bbb330e10f6cd1bb89ee6f57c54
MegaChie <workspace.mega@gmail.com>
2022-10-31 23:53:10 +0200

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *  *_strpbrk - Fills memory with a constant byte
+ * @s: pointer to where the function check
+ * @accept: pointer for checking string
+ * Return: Pointer to memory area
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i,j;
+	char *place;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				place = &s[i];
+				return (p);
+			}
+			j++
+		}
+		i++;
+	}
+	return (0);
+}
