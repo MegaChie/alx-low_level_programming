c209f084b2ced35a7fa9a49084f77a0a74918c14
MegaChie <workspace.mega@gmail.com>
2022-11-01 00:45:27 +0200

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *  *_strstr - Fills memory with a constant byte
+ * @s: pointer to where the function check
+ * @accept: pointer for checking string
+ * Return: Pointer to memory area
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, s, d = 0;
+
+	while (haystack[a] != '\0')
+	{
+		s = 0;
+		while (haystack[a + d] != '\0' && needle[s + d] != '\0' && haystack[a + d] == needle[s + d])
+		{
+			if (haystack[a + d] != needle[s + d])
+			{
+				break;
+			}
+			d++;
+		}
+		a++;
+		if (needle[s + d] == '\0')
+		{
+			break;
+		}
+		s++;
+	}
+	return (NULL);
+}
