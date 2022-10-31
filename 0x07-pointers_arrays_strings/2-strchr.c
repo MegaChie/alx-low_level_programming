25ca2f418cfe1a9487d6390f291fab8e52c46fc9
MegaChie <workspace.mega@gmail.com>
2022-10-31 17:30:55 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _memcpy - chabge value of a variable
- * @dest: variable to be changes
- * @src: rewite value of change
- * @n: number of elements to change
- * Return: pointer to `dest`
+ * *_strchr - check presence of a value of a variable
+ * @s: place to check the presense
+ * @c: the value we look for
+ * Return: pointer to value presence
  */
 char *_strchr(char *s, char c)
 {
