4e1bb5cd9964b1571627b463adf617924ee28178
MegaChie <workspace.mega@gmail.com>
2022-10-31 16:38:54 +0200

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset _ replace n bytes forom memory with b
+ * @s: pointer to char
+ * @b: rewrite value
+ * @n: number of rewrites
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+	return (s);
+}
