cfa832b00d201604aeaa91e10692bdc817233bba
MegaChie <workspace.mega@gmail.com>
2022-10-31 23:25:20 +0200

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,28 +7,5 @@
  */
 unsigned int _strspn(char *s, char *accept);
 {
-	int i;
-	int j;
-	int count;
-
-	i = 0;
-	count = 0;
-
-	while (s[i] != '\0')
-	{
-		j = 0;
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])
-			{
-				count++;
-				break;
-			}
-			j++;
-		}
-		if (accept[j] == '\0')
-			break;
-		i++;
-	}
-	return (count);
+	return (strspn(s, accept));
 }
