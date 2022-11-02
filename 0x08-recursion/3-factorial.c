ab6cb16a3e9b8123c2eac2c2fabe9f889cbff610
MegaChie <workspace.mega@gmail.com>
2022-11-02 14:46:14 +0200

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -15,8 +15,12 @@ int factorial(int n)
 	{
 		return (1);
 	}
+	else if (n == 1)
+	{
+		return (1);
+	}
 	else
 	{
-		return (factorial(n) * factorial(n - 1));
+		return (i * factorial(i - 1));
 	}
 }
