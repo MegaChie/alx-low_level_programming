18e276364e60a7481de63b73ffc62da68faecf20
MegaChie <workspace.mega@gmail.com>
2022-11-04 21:05:58 +0200

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arrgument vector
+ * return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printd("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
