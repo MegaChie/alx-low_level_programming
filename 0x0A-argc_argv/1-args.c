72b896b9ce63fb2888f723b7fad107156de8083b
MegaChie <workspace.mega@gmail.com>
2022-11-04 21:14:57 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arrgument vector
+ * Return: This return to 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argv - 1);
+	(void)argc;
+	return (0);
+}
