a0c9ea351d10a56a3c02aef30efd140fd62aaa14
MegaChie <workspace.mega@gmail.com>
2022-11-04 21:22:15 +0200

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @args: argument count
+ * @argv: arrgument vector
+ * Return: This return to 0
+ */
+int main(int args, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
