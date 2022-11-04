a30366d1a56999b120f41b0720848a317dd73f95
MegaChie <workspace.mega@gmail.com>
2022-11-04 21:33:15 +0200

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arrgument vector
+ * Return: This return to 0
+ */
+int main(int argc, char *argv[])
+{
+	int mull;
+	if (argc < 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	mul = mull[1] * mull[2];
+	printf("%d", mull);
+	return (0);
+}
