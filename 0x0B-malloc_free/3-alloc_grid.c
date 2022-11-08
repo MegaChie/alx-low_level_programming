afd67045f5ffb11f668cf111bb6149297493293e
MegaChie <workspace.mega@gmail.com>
2022-11-08 21:48:07 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -42,39 +42,4 @@ int **alloc_grid(int width, int height)
 		}
 		return (x);
 	}
-}int i, j, a, s;
-	int **x;
-
-	if (width <= 0 || height <= 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		x = (int **) malloc(height * sizeof(int *));
-		if (!x)
-		{
-			free(x);
-			return (NULL);
-		}
-		for (i = 0; i < height; i++)
-		{
-			x[i] = (int *) malloc(width * sizeof(int));
-			if (!x[i])
-			{
-				for (j = 0; j <= i; j++)
-					free(x[j]);
-				free(x);
-				return (NULL);
-			}
-		}
-		for (a = 0; a < height; a++)
-		{
-			for (s = 0; s < width; s++)
-			{
-				x[a][s] = 0;
-			}
-		}
-		return (x);
-	}
 }
