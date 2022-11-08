17b3205aed94994406d9d9a91454d4b5bd10f519
MegaChie <workspace.mega@gmail.com>
2022-11-09 01:14:40 +0200

    Create 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * argstostr-  concatenates all the arguments of your program.
+ * alloc_grid function. integers.
+ * @ac: number of arguments
+ * @av: arry of arrguments
+ * Return: pointer or NULL
+ */
+char **strtow(char *str)
+{
+	char * token = strtok(str, " ");
+   while( token != NULL ) {
+      printf( " %s\n", token );
+      token = strtok(NULL, " ");
+   }
+}
