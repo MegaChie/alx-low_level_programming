244aa34b460751176f2ded48de177d9805a16c69
MegaChie <workspace.mega@gmail.com>
2022-11-08 12:41:25 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *x;
 
+	x = malloc(strlen(s1) + strlen(s2) + 1);
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -20,7 +21,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	x = malloc(strlen(s1) + strlen(s2) + 1);
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+	
 	strcpy(x, s1);
 	strcat(x, s2);
 	
