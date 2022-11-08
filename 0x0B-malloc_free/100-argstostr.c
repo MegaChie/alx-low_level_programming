25e8bc4d7549a8dfb1ad27afb24010338f7c2cdb
MegaChie <workspace.mega@gmail.com>
2022-11-09 00:26:49 +0200

    Create 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr-  concatenates all the arguments of your program.
+ * alloc_grid function. integers.
+ * @ac: number of arguments
+ * @av: arry of arrguments
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *x;
+
+	if (ac == NULL || av == NULL)
+	{
+		return (NULL);
+	}
+	x = malloc(sizeof(char) * (strlen(av) + 1));
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+	for(i = 1; i < argc; i++)
+	{
+    	x = strcpy(str, arg[i]);
+    	x = strcat(str, " ");
+	}
+	return (x);
+}
