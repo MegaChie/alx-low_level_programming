e481f5be700abb3e27ac53355ca8e481ba88b6f6
MegaChie <workspace.mega@gmail.com>
2022-11-11 00:09:57 +0200

    Create 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mat;
+	unsigned int a;
+
+	if (nmemb <= 0 || size <= 0)
+	{
+		return (0);
+	}
+	mat = malloc(nmemb * size);
+	if (mat == 0)
+	{
+		return (0);
+	}
+	for (a = 0; a < (nmemb * size); a++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+	return (mat);
+}
