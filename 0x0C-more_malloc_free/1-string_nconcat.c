c04e574f6638608f99a47d48db150bd1d5d632f2
MegaChie <workspace.mega@gmail.com>
2022-11-10 22:47:11 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = b;
 	}
-	new = malloc(sizeof(char) * (a + n + 1));
+	new = malloc(sizeof(*new) * (a + n + 1));
 	if (new == 0);
 	{
 		return (0);
