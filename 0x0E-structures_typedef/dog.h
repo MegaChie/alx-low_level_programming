c347df4add8ceedc3726e853a34f9ee18b4c76ae
MegaChie <workspace.mega@gmail.com>
2022-11-14 12:42:25 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
+/**
+ * struct dog - define structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * daaah
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
