7acd784b41402b17d8a193ccc3a8d3735f56f0b5
MegaChie <workspace.mega@gmail.com>
2022-11-14 14:51:00 +0200

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,31 +8,28 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
+	if (d->name == 0)
+	{
+		printf("Name: (nil)\n");
+	}
 	else
 	{
-		if (d->name == 0)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n",d->name );
-		}
-		if (d->age == 0)
-		{
-			printf("Age: (nil)");
-		}
-		else
-		{
-			printf("Age: %f\n",d->age);
-		}
-		if (d->owner == 0)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-		{
-			printf("Owner: %s\n",d->owner );
-		}
+		printf("Name: %s\n",d->name );
+	}
+	if (d->age == 0)
+	{
+		printf("Age: (nil)");
+	}
+	else
+	{
+		printf("Age: %f\n",d->age);
+	}
+	if (d->owner == 0)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n",d->owner );
 	}
 }
