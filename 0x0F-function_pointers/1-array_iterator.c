57385ee26a66d19de6e6748dd99edc10e4c3bb5f
MegaChie <workspace.mega@gmail.com>
2022-11-16 22:20:53 +0200

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,10 +5,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a;
 
-	if (size == 0 || array == 0 || action == 0)
-	{
-		return;
-	}
 	if ((array && size > 0) && action)
 	{
 		for (a = 0; a > size; a++)
