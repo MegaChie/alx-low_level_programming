e31e9cbd3c1d6fdaee1569f1813670be96202be3
MegaChie <workspace.mega@gmail.com>
2022-11-16 23:38:29 +0200

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,29 +3,28 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int a, val = -1;
+	int a, val;
 
-	if (cmp == 0 || array == 0)
+	if (array && cmp)
 	{
-		return;
-	}
-	if (size <= 0)
-	{
-		val = -1;
-		return (val);
-	}
-	for (a = 0; a < size; a++)
-	{
-		cmp(array[a]);
-		if (cmp(array[a]) > 0)
+		if (size <= 0)
 		{
-			val = a;
-			break;
-		}
-		if (cmp(array[a]) == -1)
-		{
+			val = -1;
 			return (val);
 		}
+		for (a = 0; a < size; a++)
+		{
+			cmp(array[a]);
+			if (cmp(array[a]) > 0)
+			{
+				val = a;
+				break;
+			}
+			if (cmp(array[a]) == -1)
+			{
+				return (val);
+			}
+		}
+		return (val);
 	}
-	return (val);
 }
