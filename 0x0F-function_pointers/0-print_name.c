a132b29302838e359af03a380ac3bf7687c89da9
MegaChie <workspace.mega@gmail.com>
2022-11-16 21:46:03 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * print_name - prints a name.
+ * @name: name to print
+ * @f: pointer to function
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == 0 || f == 0)
