c3bf27453feec62229f9192f6e53cab7ffdcc882
MegaChie <workspace.mega@gmail.com>
2022-11-16 21:49:49 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
+#ifndef function_pointer_h
+#define function_pointer_h
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char);
+#endif
