561199785c7c1c86605925167f91d2287b4ae1c0
MegaChie <workspace.mega@gmail.com>
2022-11-17 14:41:39 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdarg.h>
+
 void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
+
 /**
  * print_char - Prints a char.
- * @arg: pointer to printable
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
  */
 void print_char(va_list arg)
 {
@@ -16,9 +19,11 @@ void print_char(va_list arg)
 	letter = va_arg(arg, int);
 	printf("%c", letter);
 }
+
 /**
  * print_int - Prints an int.
- * @arg: pointer to printable
+ * @arg: A list of arguments pointing to
+ *       the integer to be printed.
  */
 void print_int(va_list arg)
 {
@@ -27,9 +32,11 @@ void print_int(va_list arg)
 	num = va_arg(arg, int);
 	printf("%d", num);
 }
+
 /**
  * print_float - Prints a float.
- * @arg: pointer to printable
+ * @arg: A list of arguments pointing to
+ *       the float to be printed.
  */
 void print_float(va_list arg)
 {
@@ -38,9 +45,11 @@ void print_float(va_list arg)
 	num = va_arg(arg, double);
 	printf("%f", num);
 }
+
 /**
  * print_string - Prints a string.
- * @arg: pointer to printable
+ * @arg: A list of arguments pointing to
+ *       the string to be printed.
  */
 void print_string(va_list arg)
 {
@@ -56,30 +65,37 @@ void print_string(va_list arg)
 
 	printf("%s", str);
 }
+
 /**
  * print_all - Prints anything, followed by a new line.
  * @format: A string of characters representing the argument types.
+ * @...: A variable number of arguments to be printed.
+ *
+ * Description: Any argument not of type char, int, float,
+ *              or char * is ignored.
+ *              If a string argument is NULL, (nil) is printed instead.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0, j = 0;
 	char *separator = "";
-	printer_t funcs[] =
-	{
+	printer_t funcs[] = {
 		{"c", print_char},
 		{"i", print_int},
 		{"f", print_float},
 		{"s", print_string}
 	};
+
 	va_start(args, format);
+
 	while (format && (*(format + i)))
 	{
 		j = 0;
+
 		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-		{
 			j++;
-		}
+
 		if (j < 4)
 		{
 			printf("%s", separator);
@@ -89,6 +105,8 @@ void print_all(const char * const format, ...)
 
 		i++;
 	}
+
 	printf("\n");
+
 	va_end(args);
 }
