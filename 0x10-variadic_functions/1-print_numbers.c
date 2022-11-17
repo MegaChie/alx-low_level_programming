828ef9ace2a934ac01a6497a54e55830aa0a09a8
MegaChie <workspace.mega@gmail.com>
2022-11-17 13:25:41 +0200

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,10 @@
 #include <stdarg.h>
 #include <stdio.h>
-
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: what to type inbetween numbers
+ * @n: numbers to type
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list num;
