ce7dde58b7f01d80e926796f8cb7b82a309f522a
MegaChie <workspace.mega@gmail.com>
2022-11-26 00:20:31 +0200

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+/**
+ * print_list - prints all elements of list_t.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t a;
+
+	a = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
