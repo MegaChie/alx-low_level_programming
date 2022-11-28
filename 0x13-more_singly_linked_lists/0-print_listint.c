f03c3af047a5edfba1dd38b82cbbd4d760df0387
MegaChie <workspace.mega@gmail.com>
2022-11-28 20:56:49 +0200

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * 
+ */
+size_t print_listint(const listint_t *h)
+{
+	int a = 0;
+	while(node != NULL)
+    {
+		printf("%d ",node->data);
+		node = node->next;
+		printf("\n");
+		a++;
+    }
+return (a);
+}
