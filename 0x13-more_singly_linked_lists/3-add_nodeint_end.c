10bdcdcf644fd8aa5bc34fafae5151a7e9936349
MegaChie <workspace.mega@gmail.com>
2022-11-28 22:35:23 +0200

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,7 +24,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     {
         while (place->next != 0)
         {
-            place->next = next;
+            place = place->next;
         }
         place->next = newNode;
     }
