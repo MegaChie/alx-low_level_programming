246c99d8a9306f3d99d3c8f909546b50b1fe7ef8
MegaChie <workspace.mega@gmail.com>
2022-11-29 22:19:36 +0200

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,6 +4,7 @@
  * a pointer to it.
  * @head: pointer to list
  * @index: number of node to be found in the linked list
+ * Return: pointer to found node or 0
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
