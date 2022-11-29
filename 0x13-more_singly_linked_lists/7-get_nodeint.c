5faec5587f0b9d7d58fdb0bb83dbe0f49e372659
MegaChie <workspace.mega@gmail.com>
2022-11-29 22:03:28 +0200

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * 
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t dest;
+
+	for (dest = 0; dest < index && head->next; des++)
+	{
+		head =head->next;
+	}
+	if (dest < index)
+	{
+		return (0);
+	}
+	return (head);
+}
