aafc79c4d81aafc1d75c494b6d05079d9e43a62c
MegaChie <workspace.mega@gmail.com>
2022-11-29 22:15:02 +0200

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (dest = 0; dest < index && head->next; dest++)
 	{
-		head =head->next;
+		head = head->next;
 	}
 	if (dest < index)
 	{
