40f678f1b521da8e8093b2f7809afbc3cf8a2027
MegaChie <workspace.mega@gmail.com>
2022-11-29 20:40:22 +0200

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 /**
- * 
+ * free_listint - free memory of list.
+ * @head: pointer to head of list
  */
 void free_listint(listint_t *head)
 {
-    listint_t *place;
+	listint_t *place;
 
-    while (head != 0)
-    {
-        place = head;
-        head = head->next;
-        free(place);
-    }
+	while (head != 0)
+	{
+		place = head;
+		head = head->next;
+		free(place);
+	}
 }
