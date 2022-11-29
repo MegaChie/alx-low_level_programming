c4e161dbfe1654e56e9cf30cb8be7f6a1330af3a
MegaChie <workspace.mega@gmail.com>
2022-11-29 23:45:24 +0200

    Create 103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * find_listint_loop - finds loop in linked list.
+ * @head: pointer to linked list
+ * Return: pointer to node with loop or 0
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *nodeOne, *nodeTwo;
+
+	if (head == 0 || head->next == 0)
+	{
+		return (0);
+	}
+	nodeOne = head->next;
+	nodeTwo = (head->next)->next;
+	while (nodeTwo)
+	{
+		if (nodeOne == nodeTwo)
+		{
+			nodeOne = head;
+			while (nodeOne != nodeTwo)
+			{
+				nodeOne = nodeOne->next;
+				nodeTwo = nodeTwo->next;
+			}
+			return (nodeOne);
+		}
+		nodeOne = nodeOne->next;
+		nodeTwo = (nodeTwo->next)->next;
+	}
+	return (0);
+}
