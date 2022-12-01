f8710ebcbfa85f1396d2c53a393eb20d5b527020
MegaChie <workspace.mega@gmail.com>
2022-12-01 23:57:12 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <string.h>
 /**
- * 
+ * binary_to_uint - convert binary to decimal.
+ * @b: pointer to binary.
+ * Return: convertion result
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int count = strlen(b), place = 0, decrease;
+	int count = strlen(b), place = 0;
 	unsigned int value = 0;
 
 	if (b == 0)
@@ -14,17 +16,9 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (;place < count; place++)
 	{
-		if (b[place] != 0 && b[place] != 1)
+		if (b[place] != '0' && b[place] != '1')
 		{
 			return (0);
 		}
 	}
-	decrease = count;
-	count--;
-	for (;place < decrease; place++)
-	{
-		value = value + ((2 ^ count) * b[place]);
-		count--;
-	}
-	return (value);
 }
