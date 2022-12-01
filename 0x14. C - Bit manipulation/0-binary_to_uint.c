10a8b5a21e55bff394d9a80df79cc90f570644f8
MegaChie <workspace.mega@gmail.com>
2022-12-01 19:43:43 +0200

    Create 0-binary_to_uint.c

---
+++ 0x14. C - Bit manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <string.h>
+/**
+ * 
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int count = strlen(b), place = 0;
+	unsigned int value;
+
+	if (b == 0)
+	{
+		return (0);
+	}
+	for (;place < count ; place++)
+	{
+		if (b[place] != 0 && b[place] != 1)
+		{
+			return (0);
+		}
+	}
+	for (;place < count; place++)
+	{
+		_putchar('%u', b[place] * count);
+		count--;
+	}
+}
