2d45277e860918b1bbee134670ca5c39d77f5658
MegaChie <workspace.mega@gmail.com>
2022-12-01 23:44:01 +0200

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - check indess
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}
