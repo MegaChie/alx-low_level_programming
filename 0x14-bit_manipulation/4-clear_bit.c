4d987512b7e227ce119fc5eb22a74a47f3f72b9d
MegaChie <workspace.mega@gmail.com>
2022-12-01 23:25:12 +0200

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- * 
+ * clear_bit - change a certain bit's value to 0
+ * @n: pointer to number
+ * @index: place of bit to clear
+ * Return: 1 or -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
