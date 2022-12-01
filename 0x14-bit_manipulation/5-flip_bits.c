85b0a2078f9474b8c5c549fd6ec36e8afcd64b50
MegaChie <workspace.mega@gmail.com>
2022-12-01 23:40:00 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- * 
+ * flip_bits - get numbers of bits to flip till the numbers are equal
+ * @n: first number
+ * @m: second number
+ * Return: number of bits flipped
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
