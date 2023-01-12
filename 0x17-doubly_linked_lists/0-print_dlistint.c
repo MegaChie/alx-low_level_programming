bf41a8858d0e1346dcd0d045cd7302516d00b4fc
MegaChie <workspace.mega@gmail.com>
2023-01-12 12:42:46 +0200

    Create 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * print_dlistint - check code.
+ * @h: constant struct pointer
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == 0)
+	{
+		return count;
+	}
+}
