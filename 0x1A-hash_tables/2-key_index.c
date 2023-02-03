fac85ecde46cfeb9b3b0bf5255bde88870c925fd
MegaChie <workspace.mega@gmail.com>
2023-02-03 22:26:35 +0200

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 /**
- * 
+ * key_index - check code.
+ * @hey: constant unsigned char variable
+ * @size: long unsigned integer variable
+ * Return: function's value
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
