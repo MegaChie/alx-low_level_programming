a37f4c07bbd6f1e240d07c48c4fd1694f1c73b41
MegaChie <workspace.mega@gmail.com>
2023-02-03 22:34:01 +0200

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,8 +1,12 @@
 #include "hash_tabels.h"
 /**
- * 
+ * hash_table_set - check code.
+ * @ht: hash_node_t variable
+ * @key: constant character variable
+ * @value: constant character variable
+ * Return: 0 or 1
  */
-int hash_table_set(hash_table_t *ht, sonst char *key, const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
 	char *valueCopy;
@@ -10,12 +14,12 @@ int hash_table_set(hash_table_t *ht, sonst char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)	
 	{
-		return 0;
+		return (0);
 	}
 	valueCopy = strdup(value);
 	if (valueCopy == 0)
 	{
-		return 0;
+		return (0);
 	}
 	index = key_index((const unsigned char *)key, ht->size);
 	for (count = index; ht->size[count]; count++)
@@ -24,17 +28,17 @@ int hash_table_set(hash_table_t *ht, sonst char *key, const char *value)
 		{
 			free(ht->array[count]->value);
 			ht->array[count]->value = valueCopy;
-			return 1;
+			return (1);
 		}
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == 0)
 	{
 		free(new);
-		return 0;
+		return (0);
 	}
 	new->value = valueCopy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
-	return 1;
+	return (1);
 }
