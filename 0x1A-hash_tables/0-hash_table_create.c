da71ecc9baa78e77e2b4e4252fdce7161069f24f
MegaChie <workspace.mega@gmail.com>
2023-02-03 22:04:43 +0200

    Create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * 
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashTableCreated;
+	unsigned long int count;
+
+	hashTableCreated = malloc(sizeof(hash_table_t));
+	if (hashTableCreated == 0)
+	{
+		return NULL;
+	}
+	hashTableCreated->size = size;
+	hashTableCreated->array = malloc(sizeof(hash_node_t) * size);
+	if (hashTableCreated->array == 0)
+	{
+		return NULL;
+	}
+	for (count = 0; count < size; count++)
+	{
+		hashTableCreated->array[count] = NULL;
+	}
+	return hashTableCreated;
+}
