de7c43466ec8e9ad9dce3514bdf0898a79aecdce
MegaChie <workspace.mega@gmail.com>
2023-12-18 19:58:09 +0200

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,9 +14,9 @@ int linear_search(int *array, size_t size, int value)
 	{
 		return -1;
 	}
-	for (i; i < size; i++)
+	for (; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%ld]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return i;
