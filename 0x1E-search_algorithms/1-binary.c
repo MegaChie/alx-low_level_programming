33c4e13f5d04aa246b1ce7aede8b849e184b6af6
MegaChie <workspace.mega@gmail.com>
2023-12-21 09:42:34 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,7 +8,7 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, middel, i = 0;
+	size_t left = 0, right = size - 1, middel = size, i = 0;
 
 	if (array == 0 || array == NULL)
 	{
