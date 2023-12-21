3967b83ee486162151c880e8ea31223282352258
MegaChie <workspace.mega@gmail.com>
2023-12-21 11:54:29 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,26 +1,43 @@
 #include "search_algos.h"
 /**
- * linear_search - do simple search algorithm
+ * jump_search - do simple search algorithm
  * @array: integer pointer to the first element of the array to search in
  * @size: integer variable, and is the number of elements in array
  * @value: integer variable, and is  is the value to search for
  * Return: the first index where value is located or -1
  */
-int linear_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i = 0, o = sqrt(size);
 
 	if (array == 0 || array == NULL)
 	{
 		return (-1);
 	}
-	for (; i < size; i++)
+	while (array[min(i , size) - 1] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		i = o;
+		o = o + floor(sqrt(size));
+		if (o >= size)
 		{
-			return (i);
+			return (-1);
 		}
 	}
-	return (-1);
+	while (array[i] < value)
+	{
+		o = o + 1;
+		if (o = min(o, size))
+		{
+			return (-1);
+		}
+	}
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	if (array[i] == value)
+	{
+		return (o);
+	}
+	else
+	{
+		return (-1);
+	}
 }
