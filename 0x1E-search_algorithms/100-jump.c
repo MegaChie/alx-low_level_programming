c469a4c8fe861983da0202536d3f966c73e281af
MegaChie <workspace.mega@gmail.com>
2023-12-21 15:50:14 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,7 +29,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", a, array[a]);
 		a++;
-		if (a == ((b<size) ? b : size))
+		if (a == ((b < size) ? b : size))
 		{
 			return (-1);
 		}
