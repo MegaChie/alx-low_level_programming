5b18848609d74840fee6ebbd2cc10a2881094f98
MegaChie <workspace.mega@gmail.com>
2023-12-21 14:38:44 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,7 +16,7 @@ int jump_search(int *array, size_t size, int value)
 	}
 	while (array[((b<size) ? b : size) - 1] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", a, array[a]);
+		printf("Value checked array[%ld] = [%d]\n", ((b<size) ? b : size), array[a]);
 		a = b;
 		b = b + floor(sqrt(size));
 		if (a >= size)
