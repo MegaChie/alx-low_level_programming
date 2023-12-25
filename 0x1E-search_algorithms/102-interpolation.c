5b7f334ba07f0d422ee85029b7890e40c77a28b6
MegaChie <workspace.mega@gmail.com>
2023-12-25 11:08:29 +0200

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
- * jump_search - do simple search algorithm
+ * interpolation_search - do simple interpolation algorithm
  * @array: integer pointer to the first element of the array to search in
  * @size: integer variable, and is the number of elements in array
  * @value: integer variable, and is  is the value to search for
@@ -8,5 +8,32 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	
+	size_t low = 0, high = size - 1, pos;
+
+	while ((arr[high] != arr[low]) && (value >= arr[low]) &&
+		(value <= arr[high]))
+	{
+		pos = low + (((double)(high - low) /
+			(array[high] - array[low])) * (value - array[low]));
+		if (array[pos] < value)
+		{
+			low = pos - 1;
+		}
+		else if (value < array[pos])
+		{
+			high = pos - 1;
+		}
+		else
+		{
+			return (pos);
+		}
+	}
+	if (value == array[low])
+	{
+		return (low);
+	}
+	else
+	{
+		return (-1);
+	}
 }
