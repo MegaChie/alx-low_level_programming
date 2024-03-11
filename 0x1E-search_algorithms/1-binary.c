d30bb00487e7b91d18636534fd3f3fc803d34678
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-11 21:41:55 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,3 +1,24 @@
+#include "search_algos.h"
+/**
+ * printer - Prints the array from the function bellow.
+ * @array: The array to print.
+ * @left: The left index of the array.
+ * @right: The right index of the array.
+ */
+void printer(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	if (array)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < left + (right - left + 1); i++)
+		{
+			printf("%d%s", *(array + i), i < left + (right - left) ? ", " : "\n");
+		}
+	}
+}
+
 /**
  * binary_search - do simple search algorithm
  * @array: integer pointer to the first element of the array to search in
@@ -6,3 +27,25 @@
  * Return: the first index where value is located or -1
  */
 int binary_search(int *array, size_t size, int value)
+{
+	size_t right = size - 1, left = 0, middle;
+
+	while (left <= right)
+	{
+		printer(array, left, right);
+		middle = floor((left + right) / 2);
+		if (array[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else if (array[middle] > value)
+		{
+			right = middle - 1;
+		}
+		else
+		{
+			return (middle);
+		}
+	}
+	return (-1);
+}
