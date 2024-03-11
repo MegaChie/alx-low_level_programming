764daf3198894839ffa5d4b7005243a46f1d95a0
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-11 15:58:23 +0200

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,5 +6,26 @@
  * @value: integer variable, and is  is the value to search for
  * Return: the first index where value is located or -1
  */
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
+    size_t n = 0;
+
+    // checking if the array is empty or not even there
+    if (array == 0 || array == Null)
+    {
+        return (-1);
+    }
+
+    // start the search
+    for (; n < size; n++)
+    {
+        printf("Value checked array[%ld] = [%d]", n, array[n])
+        nf (array[n] == value)
+        {
+            return (n);
+        }
+    }
+
+    // not found
+    return (-1);
 }
