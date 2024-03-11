a4a059d5c5b41299f74ac7ffdbab7f997d08ab2f
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-12 00:02:04 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,12 +1,12 @@
 #include "search_algos.h"
 /**
- * interpolation_search - do simple interpolation algorithm
+ * jump_search - do simple search algorithm
  * @array: integer pointer to the first element of the array to search in
  * @size: integer variable, and is the number of elements in array
  * @value: integer variable, and is  is the value to search for
  * Return: the first index where value is located or -1
  */
-int interpolation_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
     size_t a = 0, b = floor(sqrt(size));
 
