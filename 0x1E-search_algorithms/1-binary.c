a297239b749046b7756c17804dfec4e726bab791
MegaChie <workspace.mega@gmail.com>
2024-03-11 07:43:55 -0700

    Add 1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,8 @@
+/**
+ * binary_search - do simple search algorithm
+ * @array: integer pointer to the first element of the array to search in
+ * @size: integer variable, and is the number of elements in array
+ * @value: integer variable, and is  is the value to search for
+ * Return: the first index where value is located or -1
+ */
+int binary_search(int *array, size_t size, int value)
