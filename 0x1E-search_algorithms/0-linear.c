e88da42812041a9432fd5b6f564f5b3cd731c467
MegaChie <workspace.mega@gmail.com>
2024-03-11 06:38:22 -0700

    Add 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,10 @@
+#include "search_algos.h"
+/**
+ * linear_search - do simple search algorithm
+ * @array: integer pointer to the first element of the array to search in
+ * @size: integer variable, and is the number of elements in array
+ * @value: integer variable, and is  is the value to search for
+ * Return: the first index where value is located or -1
+ */
+int linear_search(int *array, size_t size, int value){
+}
