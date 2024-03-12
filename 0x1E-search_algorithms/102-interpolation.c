3fc5d1b9a0f4f466e9140392e4afa1202fddeb7b
MegaChie <workspace.mega@gmail.com>
2024-03-12 03:06:58 -0700

    Add 102-interpolation.c

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,9 @@
+#include "search_algos.h"
+/**
+ * interpolation_search - do simple interpolation algorithm
+ * @array: integer pointer to the first element of the array to search in
+ * @size: integer variable, and is the number of elements in array
+ * @value: integer variable, and is  is the value to search for
+ * Return: the first index where value is located or -1
+ */
+int interpolation_search(int *array, size_t size, int value)
